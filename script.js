a7b16623b2735c09a7bab9ba5c8ed20e93bfef7f
manishkumawat4113-art <manishkumawat4113@gmail.com>
2026-07-31 16:12:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2017,7 +2017,7 @@ if (message.edited === true) {
 div.appendChild(
     textSpan
 );
- let timeSpan =
+/* let timeSpan =
     document.createElement(
         "span"
     );
@@ -2038,7 +2038,7 @@ timeSpan.textContent =
 
 div.appendChild(
     timeSpan
-);   
+);   */
 
 
     // ========================================================
