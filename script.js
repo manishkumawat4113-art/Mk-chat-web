6cd7a6308999435e258de48559ba0297f1aff2a9
manishkumawat4113-art <manishkumawat4113@gmail.com>
2026-08-01 16:18:15 +0530

    Comment out chat screen display line
    Comment out the line that hides the chat screen.

--- script.js
+++ script.js
@@ -3647,8 +3647,8 @@ div.addEventListener(
                     "message";
 
 
-                chatScreen.style.display =
-                  "none";
+              /*  chatScreen.style.display =*/
+                  /*"none";*/
 
 
                 profileScreen.style.display =
